28ad6db92292f3d4d776870ba471533578b9e55e
Jvitorsantos07 <joaovitorsantosdearaujo07@gmail.com>
2025-03-18 23:09:50 +0000

    Desafio Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,40 +1,64 @@
 #include <stdio.h>
-    
     int main(){
-
+         
         //Cadastro das variáveis//
 
+         int i1 = 1, i2 = 1, i3;
+         int j = 1;
+
+
+         //Peça Torre, utilizando while//
+
+        printf("***Torre*** \n");
+
+         while (i1 <= 5)
+         {
+             printf("Direita...\n");
+ 
+             i1++;
+         }
+ 
+         printf("\n"); //pular uma linha//
+ 
+         //Peça Bispo, utilizando do-while//
+
+         printf("***Bispo***\n");
+
+         do
+         {
+             printf("Diagonal, Cima, Direita...\n");
+             i2++;
+         } while (i2 <= 5);
+ 
+         printf("\n"); // Pular uma linha//
+ 
+         //Peça Rainha, utilizando for//
+ 
+         printf("***Rainha***\n");
+
+         for ( int i3 = 1; i3 <= 8; i3++)
+         {
+             printf("Esquerda...\n");
+         }
+    
+         printf("\n");
+
+         //Peça Cavalo, utilizando Loops Aninhados for e while//
         
-        int i1 = 1, i2 = 1;
+         printf("***Cavalo***\n");
 
-
-        //Peça Torre, utilizando while//
-
-        while (i1 <= 5)
+        for ( i3 = 1; i3 <= 1; i3++)
         {
-            printf("Torre - direita...\n");
-
-            i1++;
-        }
-
-        printf("\n"); //pular uma linha//
-
-        //Peça Bispo, utilizando do-while//
-
-        do
-        {
-            printf("Bispo - diagonal, cima, direita...\n");
-            i2++;
-        } while (i2 <= 5);
-
-        printf("\n"); // Pular uma linha//
-
-        //Peça Rainha, utilizando for//
-
-        for ( int i3 = 1; i3 <= 8; i3++)
-        {
-            printf("Rainha - esquerda...\n");
+            while (j <= 2)
+            {
+                printf("Baixo...\n");
+                j++;
+            }
+            
+            printf("Esqueda...\n");
         }
         
+        printf("\n");
+
         return 0;
-    }
+    } 
